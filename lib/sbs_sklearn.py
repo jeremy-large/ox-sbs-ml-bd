c46b4c579eb0f5b449e1600cc93fae07468e6db2
jeremy_large <jeremylarge@yahoo.com>
2020-04-17 09:06:04 +0100

    .

--- lib/sbs_sklearn.py
+++ lib/sbs_sklearn.py
@@ -28,54 +28,66 @@ def get_standard_data():
     return df, invalids, invoices
 
 
-def train_n_test(Xm, y, n_folds, verbose=True):
+def train_n_test(X, y, n_folds, update_frequency=None, model=None, metric=None, train_on_minority=False):
     """
-    @param Xm: rectangle of feature data
-    @param y: list or array of target data
+    @param X: a pandas DataFrame of features data of shape (A, B)
+    @param y: a pandas Series of target data, of length A
     @param n_folds: the number of splits, or folds, of the data that we would like performed
-    @param verbose: Boolean. If True, report on intermediate steps
+    @param update_frequency: after implementing this many folds, provide an update
+    @param model: by default LinearRegression(). Can also be set to another model with methods .fit() and .predict()
+    @param metric: by default metrics.r2_score. Can also be set to another metric
+    @param train_on_minority: if set to True, then reverse the roles of test and train
     @return : a list of floats, each is the test R2 from a fold of the data
     """
+    update_frequency = update_frequency or len(y) - 1
+    model = LinearRegression() if model is None else model
+    metric = metrics.r2_score if metric is None else metric
+    y = y.values.ravel()   # strip out from y just its float values as an array
+    Xm = X.values          # strip out from X just the array of data it contains
 
     kfold = KFold(n_splits=n_folds, shuffle=True)
-    splits = kfold.split(Xm, y)
 
     scores = []
-    for (train, test) in splits:
+    for (train, test) in kfold.split(Xm, y):
 
-        if verbose or (scores == []):
-            print(f"In study {len(scores) + 1}/{n_folds}, {len(test)} datapoints were held back for testing; "
-                  f"first 10 such points = {test[:10]}")
+        if train_on_minority:
+            train, test = (test, train)    # swap em!
 
-        reg = LinearRegression()
-        reg.fit(Xm.iloc[train], y.iloc[train])
+        if len(scores) % update_frequency == 0:
+            logging.info(
+                f"In study {len(scores) + 1}/{n_folds}, train on {len(train)} points; then test on the other {len(test)}: "
+                f"first few test points = {test[:5]} ")
 
-        r2 = metrics.r2_score(  # r2_score() takes two arguments ...
-            y.iloc[test],  # the actual targets, and ...
-            reg.predict(Xm.iloc[test])  # the fitted targets.
-        )  # We get a number between 0 and 1 back
+        model.fit(Xm[train], y[train])
 
-        scores.append(r2)
+        score = metric(                              # r2_score(), and similar metrics, takes two arguments ...
+                       y[test],                 # the actual targets, and ...
+                       model.predict(Xm[test])  # the fitted targets.
+                      )                              # We get a number (maybe between 0 and 1) back
+
+        scores.append(score)
 
     return scores
 
 
-def mfe_r2_diag(data_array, histogram=False):
+def mfe_r2_diag(x, histogram=False, metric_name=None, n_bins=30):
     """
-    @param data_array: a list or array of quality data from a series of statistical fits
+    @param x: a list or array of quality data from a series of statistical fits
     @param histogram: Boolean. If True, plot a histogram, otherwise plot a scatter of all the data
+    @param metric_name: an optional string which is the name of the metric, a list of which is in x
     @return : nothing
     """
-    n = len(data_array)
+
+    metric_name = metric_name or 'R2'
+
     if histogram:
-        plt.hist(data_array, bins=30)
-        plt.axvline(plt.mean(data_array), color='r')
+        plt.hist(x, bins=n_bins)
+        plt.axvline(np.mean(x), color='r')
     else:
-        plt.scatter(range(n), data_array, marker='.')
-        plt.axhline(np.mean(data_array), color='r')
-    plt.xlabel(f"The {n} different k-fold splits")
-    plt.title("R2, as calculated *only* on the testing datapoints")
-    plt.grid()
-    plt.title(f'Average value of {np.round(np.mean(data_array), 3)} shown in red')
-    plt.axhline(0, color='k')
-    plt.show()
+        plt.scatter(range(len(x)), x, marker='.')
+        plt.axhline(np.mean(x), color='r')
+
+    plt.xlabel(f'Average value of the data, {np.round(np.mean(x), 3)}, is shown in red')
+    plt.title(f"{metric_name}, as calculated *only* on the testing datapoints from {len(x)} different k-fold splits")
+
+    plt.grid(); plt.axhline(0, color='k'); plt.show()
